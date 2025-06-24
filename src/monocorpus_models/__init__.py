00bebbb5ecadca47804d06b4685227664a78abfa
tagay1n <rkharisov.tt@gmail.com>
2025-06-24 12:43:18 +0300

    Changed for OAuth token to service account token

--- src/monocorpus_models/__init__.py
+++ src/monocorpus_models/__init__.py
@@ -1,52 +1,25 @@
 import os
 
 import google.auth.transport.requests
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from sqlalchemy import Column, Integer, DateTime, String, Boolean
 from sqlalchemy import select, insert, update, delete
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Session as _Session
 from sqlalchemy.sql import func
-import threading
-
 
 # The OAuth 2.0 scopes we need.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
 
-def get_credentials(credentials_file='credentials.json', token_file='token.json'):
-    """Obtain OAuth 2.0 credentials or refresh token if expired."""
-
-    creds = None
-
-    # Load token if it exists
-    if os.path.exists(token_file):
-        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
-
-    # If there are no (valid) credentials available, let the user log in
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            # Refresh the access token using the refresh token
-            creds.refresh(google.auth.transport.requests.Request())
-        else:
-            # If no valid credentials available, initiate OAuth2 login
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_file, SCOPES)
-            creds = flow.run_local_server(access_type='offline', prompt='consent')
-
-        # Save the credentials for future use
-        with open(token_file, 'w') as token:
-            token.write(creds.to_json())
-
-    if not creds.refresh_token:
-        raise ValueError('No refresh token found in credentials')
-    return creds
-
+def get_credentials(token_file='token.json'):
+    if not os.path.exists(token_file):
+        raise ValueError(f"Token file '{token_file}' does not exist. Please authenticate first.")
+    return Credentials.from_service_account_file(token_file)
 
 class Base(DeclarativeBase):
     pass
-
 
 class Document(Base):
     """
